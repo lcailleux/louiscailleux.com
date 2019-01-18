ed46d2746c9d8d465a8b7d63880d5a5a56077584
lue <lcailleux@expertime.com>
2019-01-18 14:05:29 +0900

    Removing url for contact list

--- louiscailleux/contact/urls.py
+++ louiscailleux/contact/urls.py
@@ -5,6 +5,5 @@ from contact import views
 app_name = 'contact'
 urlpatterns = [
     url('^$', views.ContactView.as_view(), name='view'),
-    url('post', views.submit, name='submit'),
-    url('list', views.ContactListCreate.as_view(), name='list'),
+    url('post', views.submit, name='submit')
 ]
