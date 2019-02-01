fb0dcb16965fed3751fc37e309168e937a3cabd8
lue <lcailleux@expertime.com>
2019-02-01 11:59:58 +0900

    Updating block api

--- louiscailleux/cms/urls.py
+++ louiscailleux/cms/urls.py
@@ -1,11 +1,12 @@
 # contact/urls.py
 from rest_framework import routers
-from .views import BlockReadOnlyViewSet
+from .views import BlockReadOnlyViewSet, BlockIdentifierDetailViewSet
 
 app_name = 'cms'
 
 router = routers.SimpleRouter()
 router.register(r'V1/api/block', BlockReadOnlyViewSet)
+router.register(r'V1/api/block/identifier', BlockIdentifierDetailViewSet)
 
 urlpatterns = []
 urlpatterns += router.urls
--- louiscailleux/cms/views.py
+++ louiscailleux/cms/views.py
@@ -1,7 +1,13 @@
 # cms/views.py
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from .models import Block
 from .serializers import BlockSerializer
+
+
+class BlockIdentifierDetailViewSet(mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+    lookup_field = 'identifier'
+    queryset = Block.objects.all()
+    serializer_class = BlockSerializer
 
 
 class BlockReadOnlyViewSet(viewsets.ReadOnlyModelViewSet):
