c7d59991149eb7faf77531455ab0cd41e12d49d9
lue <lcailleux@expertime.com>
2019-02-07 15:55:36 +0900

    Updating footer links

--- frontend/src/components/footer-info.js
+++ frontend/src/components/footer-info.js
@@ -1,12 +1,10 @@
 import React, {Component} from 'react';
 
 import Footer from 'react-bulma-components/lib/components/footer';
-import Container from 'react-bulma-components/lib/components/container';
 import Content from 'react-bulma-components/lib/components/content';
 import Hero from 'react-bulma-components/lib/components/hero';
 
 import i18n from '../i18n'
-import {about, contact} from "../helpers/urls";
 import SocialLinks from "./social-links";
 
 class FooterInfo extends Component {
@@ -20,18 +18,10 @@ class FooterInfo extends Component {
             <Hero size="fullheight">
                 <Hero.Footer>
                     <Footer>
-                        <Container >
-                            <Content>
-                                <ul>
-                                    <li><a href={about.url}>{i18n.t("About")}</a></li>
-                                    <li><a href={contact.url}>{i18n.t(contact.name)}</a></li>
-                                </ul>
-                            </Content>
-                            <Content style={{ textAlign: 'center'}}>
-                                <SocialLinks/>
-                                <p>Copyright © {this.year} Louis Cailleux. {i18n.t('All rights reserved.')}</p>
-                            </Content>
-                        </Container>
+                        <Content style={{ textAlign: 'center'}}>
+                            <SocialLinks/>
+                            <p>Copyright © {this.year} Louis Cailleux. {i18n.t('All rights reserved.')}</p>
+                        </Content>
                     </Footer>
                 </Hero.Footer>
             </Hero>
