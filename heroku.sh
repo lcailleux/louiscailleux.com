fbc713636a4a394708f93fbccf01b58ed72297e8
lue <louiscailleux@gmail.com>
2019-03-06 23:37:43 +0900

    Updated heroku configuration.

--- heroku.sh
+++ heroku.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 heroku create louiscailleux-backend-staging --region=eu --manifest;
-heroku addons:create jawsdb-maria
+heroku addons:create jawsdb-maria:kitefin
 heroku stack:set container --app louiscailleux-backend-staging;
 git add *;
 git commit -m "Updated heroku configuration."
