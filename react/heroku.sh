9cf91b7bb9abf4d1b1b3879e49c622c76dbbebcb
lue <louiscailleux@gmail.com>
2019-03-06 21:09:46 +0900

    Updated heroku configuration.

--- react/heroku.sh
+++ react/heroku.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-heroku create louiscailleux_frontend-staging --region=eu --manifest;
+heroku create louiscailleux-frontend-staging --region=eu --manifest;
 heroku stack:set container;
-git add heroku.yml;
+git add *;
 git commit -m "Updated heroku configuration."
 git push origin client;
-git push heroku client;
+git push heroku master;
