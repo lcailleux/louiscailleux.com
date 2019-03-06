350fc4d8dcd496f75dc73d37243f6ca41548b57f
lue <louiscailleux@gmail.com>
2019-03-06 20:58:16 +0900

    Removing heroku configuration since they must be independent for each app.

--- heroku.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-heroku create louiscailleux-staging --region=eu --manifest;
-heroku stack:set container;
-git add *;
-git commit -m "Updated heroku configuration."
-git push origin master;
-git push heroku master;
-
