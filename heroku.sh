449da16123416337b8deb11f8ab8730c9376cedf
lue <louiscailleux@gmail.com>
2019-03-08 16:33:20 +0900

    Updated heroku configuration.

--- heroku.sh
+++ heroku.sh
@@ -6,8 +6,7 @@ heroku local:run python manage.py migrate
 python django/louiscailleux/manage.py collectstatic
 
 heroku create louiscailleux-staging --region=eu;
-heroku addons:create jawsdb-maria:kitefin
-heroku stack:set container --app louiscailleux-staging;
+#heroku addons:create jawsdb-maria:kitefin
 
 git add *;
 git commit -m "Updated heroku configuration."
