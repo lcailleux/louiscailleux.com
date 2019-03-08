61d308061ff3a8655f9ff6830037f4930f686ccf
lue <louiscailleux@gmail.com>
2019-03-08 19:24:36 +0900

    Updated heroku configuration.

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: honcho -f honcho.conf start
+release: cd django/louiscailleux && python manage.py migrate
+web: cd django/louiscailleux && gunicorn louiscailleux.wsgi --log-file -
--- heroku.sh
+++ heroku.sh
@@ -3,7 +3,7 @@ heroku create louiscailleux-staging --region=eu --remote staging
 heroku buildpacks:add --index 1 heroku/nodejs --app louiscailleux-staging
 heroku buildpacks:add --index 2 heroku/python --app louiscailleux-staging
 
-heroku addons:remove heroku-postgresql:hobby-dev
+heroku addons:destroy heroku-postgresql:hobby-dev
 heroku addons:create jawsdb-maria:kitefin
 
 git add *;
