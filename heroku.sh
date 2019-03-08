87335f867bb1541e01221f1a552b012caa7d028b
lue <louiscailleux@gmail.com>
2019-03-08 17:01:12 +0900

    Updated heroku configuration.

--- heroku.sh
+++ heroku.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 heroku create louiscailleux-staging --region=eu --remote staging
-heroku buildpacks:add --index 1 heroku/nodejs --app louiscailleux-staging
-heroku buildpacks:add --index 2 heroku/python --app louiscailleux-staging
+heroku buildpacks:add --index 1 heroku/python --app louiscailleux-staging
 
 #heroku addons:create jawsdb-maria:kitefin
 #heroku local:run python django/louiscailleux/manage.py migrate
