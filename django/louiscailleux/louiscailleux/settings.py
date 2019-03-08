a448aa6c19b86d294cfe9884b5b21ccf55f2838e
lue <louiscailleux@gmail.com>
2019-03-08 22:29:13 +0900

    Updating whitenoise settings.

--- django/louiscailleux/louiscailleux/settings.py
+++ django/louiscailleux/louiscailleux/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -159,7 +160,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Authorizing React SPA
 CORS_ORIGIN_WHITELIST = (
