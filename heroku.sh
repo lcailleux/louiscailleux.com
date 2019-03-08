c6dd3e2fc97640d25d60810e693a2e2616fbf68f
lue <louiscailleux@gmail.com>
2019-03-08 19:56:40 +0900

    Updated heroku configuration.

--- heroku.sh
+++ heroku.sh
@@ -2,9 +2,12 @@
 heroku create louiscailleux-staging --region=eu --remote staging
 heroku buildpacks:add --index 1 heroku/nodejs --app louiscailleux-staging
 heroku buildpacks:add --index 2 heroku/python --app louiscailleux-staging
+heroku config:set DISABLE_COLLECTSTATIC=1
 
 heroku addons:destroy heroku-postgresql:hobby-dev
 heroku addons:create jawsdb-maria:kitefin
+
+
 
 git add *;
 git commit -m "Updated heroku configuration."
