c0ad4cf922530e3248189122dba1fde6bd606298
lue <louiscailleux@gmail.com>
2019-03-09 14:41:28 +0900

    Updated heroku configuration.

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: cd django/louiscailleux && python manage.py migrate
-web: cd django/louiscailleux && python manage.py migrate && gunicorn louiscailleux.wsgi --log-file -
+web: honcho -f honcho.conf start
--- heroku.sh
+++ heroku.sh
@@ -2,6 +2,7 @@
 heroku create louiscailleux-staging --region=eu
 heroku buildpacks:add --index 1 heroku/nodejs --app louiscailleux-staging
 heroku buildpacks:add --index 2 heroku/python --app louiscailleux-staging
+heroku config:set NODE_ENV=5000
 heroku config:set DISABLE_COLLECTSTATIC=1
 heroku config:set HEROKU=true
 heroku config:set PYTHONPATH=/app/.heroku/python/lib/python3.7/site-packages
