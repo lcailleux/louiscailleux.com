3a0f0f259742e9e612f8a45a4987cc2e9e3e85c0
lue <louiscailleux@gmail.com>
2019-03-10 17:15:25 +0900

    Updating production configuration.

--- heroku.sh
+++ heroku.sh
@@ -3,7 +3,7 @@
 DJANGO_SECRET_KEY=$(grep SECRET_KEY django/.env | cut -d '=' -f2)
 if [[ $1 == "staging" ]]
 then
-    python react/louiscailleux/manage.py collectstatic --noinput
+    python django/louiscailleux/manage.py collectstatic --noinput
     heroku create louiscailleux-backend-staging --region=eu --remote staging
     heroku buildpacks:set heroku/python
     heroku config:set SECRET_KEY="$DJANGO_SECRET_KEY"
@@ -15,7 +15,7 @@ then
     heroku addons:destroy heroku-postgresql:hobby-dev --confirm louiscailleux-backend-staging
 elif [[ $1 == "production" ]]
 then
-    python react/louiscailleux/manage.py collectstatic --noinput
+    python django/louiscailleux/manage.py collectstatic --noinput
     heroku create louiscailleux-backend --region=eu
     heroku buildpacks:set heroku/python
     heroku config:set SECRET_KEY="$DJANGO_SECRET_KEY"
