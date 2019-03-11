618791fee9d9c1576dd60fd3241b302b8230f7cd
lue <louiscailleux@gmail.com>
2019-03-11 14:14:33 +0900

    Removing cypress from production build.

--- heroku.sh
+++ heroku.sh
@@ -4,12 +4,14 @@ then
     heroku create louiscailleux-frontend-staging --region=eu --remote staging
     heroku buildpacks:set heroku/nodejs
     heroku config:set BACKEND_URL=https://louiscailleux-backend-staging.herokuapp.com
+    heroku plugins:install @jasonheecs/heroku-vim
     git push staging client:master;
 elif [[ $1 == "production" ]]
 then
     heroku create louiscailleux-frontend --region=eu
     heroku buildpacks:set heroku/nodejs
     heroku config:set BACKEND_URL=https://louiscailleux-backend.herokuapp.com
+    heroku plugins:install @jasonheecs/heroku-vim
     git push heroku client:master;
 else
     echo "Usage: ./heroku.sh (staging|production)"
