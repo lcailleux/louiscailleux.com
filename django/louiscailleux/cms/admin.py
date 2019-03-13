ea5d9fbbb21915f5e37d8cd2ad96f30c9d110eae
lue <louiscailleux@gmail.com>
2019-03-13 11:34:55 +0900

    Adding an admin form for the cms blocks

--- django/louiscailleux/cms/admin.py
+++ django/louiscailleux/cms/admin.py
@@ -1,3 +1,35 @@
+from django import forms
 from django.contrib import admin
 
-# Register your models here.
+from .models import Block as BlockModel
+
+
+class BlockAdminForm(forms.ModelForm):
+    LANGUAGES = (
+        ('', '(-- Please select a language --)'),
+        ('en', 'English'),
+        ('ko', 'Korean'),
+        ('fr', 'French')
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(BlockAdminForm, self).__init__(*args, **kwargs)
+        self.fields['language_code'] = forms.ChoiceField(widget=forms.Select, choices=self.LANGUAGES)
+
+    class Meta:
+        model = BlockModel
+        fields = ['language_code']
+
+
+class BlockAdmin(admin.ModelAdmin):
+    form = BlockAdminForm;
+    fieldsets = [
+        ('Contact Info', {'fields': ['title', 'identifier', 'is_active']}),
+        ('Content', {'fields': ['content', 'language_code']}),
+    ]
+    list_display = ('title', 'identifier', 'is_active', 'content', 'language_code')
+    list_filter = ['identifier']
+    search_fields = ['identifier']
+
+
+admin.site.register(BlockModel, BlockAdmin)
