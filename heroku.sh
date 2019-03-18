97531cce7392dd722558ccac05cac1d44545afe6
lue <louiscailleux@gmail.com>
2019-03-19 00:15:14 +0900

    Adding logs

--- heroku.sh
+++ heroku.sh
@@ -4,6 +4,7 @@ then
     heroku create louiscailleux-frontend-staging --region=eu --remote staging
     heroku buildpacks:set mars/create-react-app
     heroku config:set REACT_APP_BACKEND_URL=https://louiscailleux-backend-staging.herokuapp.com
+    heroku config:set REACT_APP_RECAPTCHA_KEY=6LfxHpcUAAAAAFtIyong1poMgkB_CmCXCKSU0uDT
     heroku plugins:install @jasonheecs/heroku-vim
     git push staging client:master;
 elif [[ $1 == "production" ]]
@@ -11,6 +12,7 @@ then
     heroku create louiscailleux-frontend --region=eu
     heroku buildpacks:set mars/create-react-app
     heroku config:set REACT_APP_BACKEND_URL=https://louiscailleux-backend.herokuapp.com
+    heroku config:set REACT_APP_RECAPTCHA_KEY=6LfxHpcUAAAAAFtIyong1poMgkB_CmCXCKSU0uDT
     heroku plugins:install @jasonheecs/heroku-vim
     git push heroku client:master;
 else
--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -4,13 +4,17 @@ class Constants {
     static getConstant(name) {
         const env = runtimeEnv();
 
+        console.log(env);
+
         /* Runtime variables */
         if (env[name]) {
+            console.log(env[name]);
             return env[name]
         }
 
         /* Node variables */
         if (process.env[name]) {
+            console.log(process.env[name]);
             return process.env[name];
         }
         return '';
