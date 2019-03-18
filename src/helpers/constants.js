e0c05cca5fc80cd10bd4d6a59d2c873b01155a97
lue <louiscailleux@gmail.com>
2019-03-19 02:48:36 +0900

    Removing logs

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,23 +1,7 @@
-import runtimeEnv from '@mars/heroku-js-runtime-env';
-
 class Constants {
     static getConstant(name) {
-        const env = runtimeEnv();
-
-        console.log("runtime");
-        console.log(env);
-        console.log("compiled");
-        console.log(process.env);
-
-        /* Runtime variables */
-        if (env[name]) {
-            console.log(env[name]);
-            return env[name]
-        }
-
         /* Node variables */
         if (process.env[name]) {
-            console.log(process.env[name]);
             return process.env[name];
         }
         return '';
