46f7801985af7b9dd69e2c406cc1f96199124758
lue <louiscailleux@gmail.com>
2019-03-19 01:51:53 +0900

    Updating heroku configuration.

--- heroku.sh
+++ heroku.sh
@@ -2,18 +2,14 @@
 if [[ $1 == "staging" ]]
 then
     heroku create louiscailleux-frontend-staging --region=eu --remote staging
-    heroku buildpacks:set mars/create-react-app
-    heroku config:set REACT_APP_BACKEND_URL=https://louiscailleux-backend-staging.herokuapp.com
-    heroku config:set REACT_APP_RECAPTCHA_KEY=6LfxHpcUAAAAAFtIyong1poMgkB_CmCXCKSU0uDT
-    heroku plugins:install @jasonheecs/heroku-vim
+    heroku config:set $(cat .env.staging | sed '/^$/d; /#[[:print:]]*$/d')
+    heroku buildpacks:set heroku/nodejs
     git push staging client:master;
 elif [[ $1 == "production" ]]
 then
     heroku create louiscailleux-frontend --region=eu
-    heroku buildpacks:set mars/create-react-app
-    heroku config:set REACT_APP_BACKEND_URL=https://louiscailleux-backend.herokuapp.com
-    heroku config:set REACT_APP_RECAPTCHA_KEY=6LfxHpcUAAAAAFtIyong1poMgkB_CmCXCKSU0uDT
-    heroku plugins:install @jasonheecs/heroku-vim
+    heroku config:set $(cat .env.production | sed '/^$/d; /#[[:print:]]*$/d')
+    heroku buildpacks:set heroku/nodejs
     git push heroku client:master;
 else
     echo "Usage: ./heroku.sh (staging|production)"
