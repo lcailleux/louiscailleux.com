b58d355c47fb8ce5376583e6a7b1de0b55521e90
lue <louiscailleux@gmail.com>
2019-03-18 22:17:56 +0900

    Updating permissions

--- louiscailleux/core/permissions.py
+++ louiscailleux/core/permissions.py
@@ -1,6 +1,7 @@
 from urllib.parse import urlparse
+from django.conf import settings
 from rest_framework import permissions
-from corsheaders.middleware import CorsMiddleware
+import re
 
 
 class IsRequestHostAuthorized(permissions.BasePermission):
@@ -10,7 +11,18 @@ class IsRequestHostAuthorized(permissions.BasePermission):
             return False
 
         url = urlparse(origin)
-        if CorsMiddleware.origin_found_in_white_lists(origin, url):
+        if self.origin_found_in_white_lists(origin, url):
             return True
-
         return False
+
+    def origin_found_in_white_lists(self, origin, url):
+        return (
+            url.netloc in settings.CORS_ORIGIN_WHITELIST
+            or (origin == 'null' and origin in settings.CORS_ORIGIN_WHITELIST)
+            or self.regex_domain_match(origin)
+        )
+
+    def regex_domain_match(self, origin):
+        for domain_pattern in settings.CORS_ORIGIN_REGEX_WHITELIST:
+            if re.match(domain_pattern, origin):
+                return origin
