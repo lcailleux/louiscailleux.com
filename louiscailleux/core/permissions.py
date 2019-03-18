2f5ea5062eb00a8f836fca526e2f4fa209510259
lue <louiscailleux@gmail.com>
2019-03-18 21:13:56 +0900

    Cataching Key_Errors.

--- louiscailleux/core/permissions.py
+++ louiscailleux/core/permissions.py
@@ -6,6 +6,14 @@ class IsRequestHostAuthorized(permissions.BasePermission):
     def has_permission(self, request, view):
         whitelist = settings.CORS_ORIGIN_WHITELIST
 
-        if request.META['REMOTE_HOST'] in whitelist or request.META['REMOTE_ADDR'] in whitelist:
-            return True
+        try:
+            if request.META['REMOTE_HOST'] in whitelist:
+                return True
+        except KeyError:
+            try:
+                if request.META['REMOTE_ADDR'] in whitelist:
+                    return True
+            except KeyError:
+                return False
+        
         return False
