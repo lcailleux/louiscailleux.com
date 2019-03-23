84f33b5e39319715c0fc2ce1d1e6c41421f5f316
lue <louiscailleux@gmail.com>
2019-03-23 22:59:03 +0900

    Serving robots.txt, sitemap.xml based on heroku app environment

--- server.js
+++ server.js
@@ -3,10 +3,27 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.get('/robots.txt', (req, res) => {
+ if (process.env.REACT_APP_ENV === "production") {
+  res.sendFile(path.join(__dirname, 'build', 'robots.txt'));
+ } else {
+  res.status(404).send('<pre>Cannot GET /robots.txt</pre>');
+ }
+});
+
+app.get('/sitemap.xml', (req, res) => {
+ if (process.env.REACT_APP_ENV === "production") {
+  res.sendFile(path.join(__dirname, 'build', 'sitemap.xml'));
+ } else {
+  res.status(404).send('<pre>Cannot GET /sitemap.xml</pre>');
+ }
+});
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', function (req, res) {
  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
 
 app.listen(PORT);
