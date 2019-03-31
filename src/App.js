a9ed76af706115f962ad8ed29fbc6903aaaac30f
lue <louiscailleux@gmail.com>
2019-04-01 01:11:47 +0900

    Updating index

--- src/App.js
+++ src/App.js
@@ -16,10 +16,6 @@ class App extends Component {
                 <Helmet>
                     <meta name="description" content="Louis Cailleux's portfolio"/>
                     <meta name="keyword" content="programming, python, django, react"/>
-                    <script defer src="https://use.fontawesome.com/releases/v5.7.1/js/all.js"
-                            integrity="sha384-eVEQC9zshBn0rFj4+TU78eNA19HMNigMviK/PU/FFjLXqa/GKPgX58rvt5Z8PLs7"
-                            crossOrigin="anonymous"/>
-                    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"/>
                 </Helmet>
                 <AdsInitialize />
                 <NavigationBar />
