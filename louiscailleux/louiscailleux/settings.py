375af80dfea35242b06351d0a8e5651afed6c8bd
lue <louiscailleux@gmail.com>
2019-04-07 02:44:19 +0900

    Adding gzip compression.

--- louiscailleux/louiscailleux/settings.py
+++ louiscailleux/louiscailleux/settings.py
@@ -67,6 +67,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.gzip.GZipMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
