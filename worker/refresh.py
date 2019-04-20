70b2730cd257c7ae4e6d6075230adf13f80fd3eb
lue <louiscailleux@gmail.com>
2019-04-20 14:32:19 +0900

    Updating worker.

--- worker/refresh.py
+++ worker/refresh.py
@@ -5,19 +5,27 @@ import time
 s = sched.scheduler(time.time, time.sleep)
 
 
-def refresh_site(sc):
+def refresh_backend(sc):
     backend_request = urllib.request.Request("https://louiscailleux-backend.herokuapp.com",
                                              headers={'User-Agent': "Python"})
     backend = urllib.request.urlopen(backend_request)
 
+    #if backend.getcode():
+        #print("Backend ping successfull")
+
+    s.enter(90, 1, refresh_backend, (sc,))
+
+
+def refresh_frontend(sc):
     frontend_request = urllib.request.Request("https://www.louiscailleux.com", headers={'User-Agent': "Python"})
     frontend = urllib.request.urlopen(frontend_request)
 
-    #if backend.getcode() and frontend.getcode():
-        #print("Ping successfull")
+    # if frontend.getcode():
+    # print("Frontend ping successfull")
 
-    s.enter(25, 1, refresh_site, (sc,))
+    s.enter(25, 1, refresh_frontend, (sc,))
 
 
-s.enter(25, 1, refresh_site, (s,))
+s.enter(25, 1, refresh_frontend, (s,))
+s.enter(90, 1, refresh_backend, (s,))
 s.run()
