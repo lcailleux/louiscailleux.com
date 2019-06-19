b732ecb9b79bdceeea82d513e8c834003987ea94
lue <louiscailleux@gmail.com>
2019-06-19 17:10:23 +0200

    Adding worker again.

--- worker/refresh.py
+++ worker/refresh.py
@@ -13,19 +13,8 @@ def refresh_backend(sc):
     #if backend.getcode():
         #print("Backend ping successfull")
 
-    s.enter(90, 1, refresh_backend, (sc,))
+    s.enter(25, 1, refresh_backend, (sc,))
 
 
-def refresh_frontend(sc):
-    frontend_request = urllib.request.Request("https://www.louiscailleux.com", headers={'User-Agent': "Python"})
-    frontend = urllib.request.urlopen(frontend_request)
-
-    # if frontend.getcode():
-    # print("Frontend ping successfull")
-
-    s.enter(25, 1, refresh_frontend, (sc,))
-
-
-s.enter(25, 1, refresh_frontend, (s,))
-s.enter(90, 1, refresh_backend, (s,))
+s.enter(25, 1, refresh_backend, (s,))
 s.run()
