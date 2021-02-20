06217018ef5d7422601b453d33386cc90c4238e1
lue <louiscailleux@gmail.com>
2021-02-20 16:51:16 +0100

    Updating configuration.

--- louiscailleux/louiscailleux/settings.py
+++ louiscailleux/louiscailleux/settings.py
@@ -138,13 +138,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'Europe/Paris'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
